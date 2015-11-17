cc1d1afdbe0d0ad6b69a1d9925dbf7ca3564251d
Serviox19 <servio19@gmail.com>
2015-11-17 18:32:06 -0500

    finished product

--- script.js
+++ script.js
@@ -3,15 +3,36 @@
 // click class has to count how many times its clicked //
 // # of clicks gets displayed in the "display" span //
 
-var bitcoins = document.getElementsByClass('click');
+var bitcoins = document.getElementsByClassName('click');
 
-var startGame = document.getElementByClass('start');
+var startGame = document.getElementById('start');
+
+var gameTimeout;
+
 
 var score = 0;
 
+
+
+startGame.addEventListener("click", function () {
   for(var i = 0; i < bitcoins.length; i++) {
-    bitcoins[i].addEventListener("click", function counter() {
-     score = score + 1;
-     
+    bitcoins[i].addEventListener("click", function () {
+      if(this.getAttribute("data-clicked") !== "true") {
+        score++;
+        this.setAttribute("data-clicked", "true")
+      }
+   });
+  }
+
+  gameTimeout = setTimeout(function() {
+  ("You clicked " + score + " times");
+  }, 20000);
+
+});
+
+
+
+
+
 
 
