8c8d2e040fb0f835011796edaf13f2e28f4d89a2
C-Das <chinmay98@gmail.com>
2016-01-12 17:26:56 -0500

    cleaned up HTML, CSS and JS files

--- script.js
+++ script.js
@@ -4,35 +4,19 @@
 // # of clicks gets displayed in the "display" span //
 
 var bitcoins = document.getElementsByClassName('click');
-
 var startGame = document.getElementById('start');
-
 var gameTimeout;
-
-
 var score = 0;
-
-
-
 startGame.addEventListener("click", function () {
-  for(var i = 0; i < bitcoins.length; i++) {
-    bitcoins[i].addEventListener("click", function () {
-      if(this.getAttribute("data-clicked") !== "true") {
-        score++;
-        this.setAttribute("data-clicked", "true")
-      }
-   });
-  }
-
-  gameTimeout = setTimeout(function() {
-  result.innerHTML=score;
-  }, 20000);
-
+	for(var i = 0; i < bitcoins.length; i++) {
+		bitcoins[i].addEventListener("click", function () {
+			if(this.getAttribute("data-clicked") !== "true") {
+				score++;
+				this.setAttribute("data-clicked", "true")
+			}
+		});
+	}
+	gameTimeout = setTimeout(function() {
+		result.innerHTML=score;
+	}, 20000);
 });
-
-
-
-
-
-
-
